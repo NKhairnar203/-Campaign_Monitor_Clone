d9f17426920736a0987a9c08dbf8bd04b36a3af3
Nilesh Khairnar <139966220+NKhairnar203@users.noreply.github.com>
2023-12-09 17:37:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+let menu_icon = document.querySelector("#menu-click");
+
+
+menu_icon.addEventListener('click',function(){
+    alert('click done')
+    
+})
+
+
+let btn1 = document.querySelector('#sign-up-for-free');
+btn1.addEventListener('click',function(){
+    window.open("./signup_page.html" )
+})
+
+let head_btn = document.querySelector('#try-it')
+head_btn.addEventListener('click',function(){
+    window.open("./signup_page.html" )
+})
+
+let get_btn = document.querySelector('#get')
+get_btn.addEventListener('click',function(){
+    window.open("./signup_page.html" )
+})
+
+let call_btn = document.querySelector('#signup-2')
+call_btn.addEventListener('click',function(){
+    window.open("./signup_page.html" )
+})
